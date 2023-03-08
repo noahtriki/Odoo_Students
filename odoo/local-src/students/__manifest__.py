db130bee72cf4fa7e85ad8085655bf3f0f2ae0e4
matski <noah.triki@etu.univ-savoie.fr>
2023-03-08 17:47:49 +0100

    liaou

---
+++ odoo/local-src/students/__manifest__.py
@@ -0,0 +1,11 @@
+{
+ "name": "Gestion des étudiants",
+ "version": "0.7",
+ "category": "Generic Modules/Others",
+ "description": """Test création module gestion des étudiants Odoo v14""",
+ "author": "Triki",
+ "data": ["views/students_views.xml"],
+ "depends": ["base"],
+ "installable": True,
+ "auto_install": False,
+}
